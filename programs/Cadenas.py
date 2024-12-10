ea30ce6642665cc2a83a9490c95d442238369a6d
Maxin <maximiliano.mtz.gnlz@gmail.com>
2024-12-09 21:44:04 -0600

    Comentarios agg

--- programs/Cadenas.py
+++ programs/Cadenas.py
@@ -1,8 +1,8 @@
 # Estas son cadenas de texto
 print("Hello World")
-
+# En Python podemos usar ambas comillas "dobles" y 'simples'
 print('Este es un texto que incluye ""')
-
+# Se pueden alternar ambas comillas para mostrarlas y viceversa, esto es algo unico de python
 print("Esta 'palabra' se encuentra escrita entre comillas simples")
 
 # Secuencias de escape
@@ -14,27 +14,32 @@ print("Un texto \t una tabulacion")
 print("Un testo \n desde un salto de linea")
 print("C:\nombre\directorio")
 
-# print(r"C:\nombre\directoriox\")
+#! Nota: Esto tiene que ser ejecutado en la terminal, o en la version 9 o 10 de python3 
+# # print(r"C:\nombre\directoriox\")
 # print("""Una linea)
 
 c = "Esto es una cadena \n de dos lineas"
 print(c)
-type(c)
+type(c)     #! NOTA: el type() se usa para conocer el tipo de dato de dicha variable
 
 #  Operaciones
 c = "Esto es una cadena\n con dos lineas"
 print(c + c)
 
+# Esta es la forma de hacer una cadena compuesta en una variable string
 s = "una cadena" "compuesta de dos cadenas"
 print(s)
 
+# Igual pueden sumar variables string
 c1 = "Una cadena"
 c2 = "Otra cadena"
 print("Una cadena" + c2)
 
+# Para imprimir una cadena n veces se usa el * como multiplicador en vez de un array
 then_spaces = " " *10
 print(then_spaces + "Un texto a diez espacios")
 
+# Otra forma de multiplicar tus cadenas
 m = "Hola"
 print(m*10)
 
--- programs/helloWorld.py
+++ programs/helloWorld.py
@@ -1,3 +1,5 @@
+# Para mostrar en pantalla se usa el print()
+
 print("Hello world")
 
 # Se pueden distinguir 2 tipos de numeros:
@@ -13,12 +15,15 @@ print("Hello world")
 o = (3 - 2 + 4 * 10)
 print(o)
 
+# Asignacion de un valor a n
 n=3
 print(n)
 
+# Operacion de suma con la variable n
 n + 3
 print(n)
 
+# Asignacion de valores enteros y posteriormente, mostrarlos en pantalla
 n = 18
 m = 15
 print(f"this is a int values {n} {m}")
